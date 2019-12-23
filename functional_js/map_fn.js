ad9c30c29ff50e807d3180584c0f5b83879e16ba
brianbirir <brianbirir@gmail.com>
2019-12-24 01:19:12 +0300

    fix(formatting): eslint fixes
    - semi-colons
    - arrow fn parentheses

--- functional_js/map_fn.js
+++ functional_js/map_fn.js
@@ -1,4 +1,4 @@
-const numbers = [2, 4, 8, 10]
-const halves = numbers.map(x => x / 2) // reduce numbers by half
+const numbers = [2, 4, 8, 10];
+const halves = numbers.map((x) => x / 2); // reduce numbers by half
 
-console.log(halves)
+console.log(halves);
